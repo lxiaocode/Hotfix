a6bb66132ff35fde295b0db80959e1d80fdae2c3
lxiaocode <lxiaocode@qq.com>
2023-06-09 14:48:12 +0800

    调整目录结构

---
+++ Assets/Core/Manager/VerisonMgr/VersionMgr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionMgr : MonoBehaviour
+{
+    private static string serverPatchUpdateInfoFormat = "";
+    private static string runtimePatchPathFormat = Application.persistentDataPath + "/Version_{0}";
+    private static string zipPathcFormat = "";
+    
+}
